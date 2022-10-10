4fa54b63d746652f43aa57d7a9dbbcc8808f7e72
jan cahlik <honzicekcahlicek1@gmail.com>
2022-10-10 19:58:25 +0200

    versioned

---
+++ lib/domain/note/note_failure.freezed.dart
@@ -0,0 +1,558 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'note_failure.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$NoteFailure {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() unexpected,
+    required TResult Function() insufficientPermission,
+    required TResult Function() unableToUpdate,
+    required TResult Function() unableToDelete,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Unexpected value) unexpected,
+    required TResult Function(InsufficientPermission value)
+        insufficientPermission,
+    required TResult Function(UnableToUpdate value) unableToUpdate,
+    required TResult Function(UnableToDelete value) unableToDelete,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $NoteFailureCopyWith<$Res> {
+  factory $NoteFailureCopyWith(
+          NoteFailure value, $Res Function(NoteFailure) then) =
+      _$NoteFailureCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$NoteFailureCopyWithImpl<$Res> implements $NoteFailureCopyWith<$Res> {
+  _$NoteFailureCopyWithImpl(this._value, this._then);
+
+  final NoteFailure _value;
+  // ignore: unused_field
+  final $Res Function(NoteFailure) _then;
+}
+
+/// @nodoc
+abstract class _$$UnexpectedCopyWith<$Res> {
+  factory _$$UnexpectedCopyWith(
+          _$Unexpected value, $Res Function(_$Unexpected) then) =
+      __$$UnexpectedCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$UnexpectedCopyWithImpl<$Res> extends _$NoteFailureCopyWithImpl<$Res>
+    implements _$$UnexpectedCopyWith<$Res> {
+  __$$UnexpectedCopyWithImpl(
+      _$Unexpected _value, $Res Function(_$Unexpected) _then)
+      : super(_value, (v) => _then(v as _$Unexpected));
+
+  @override
+  _$Unexpected get _value => super._value as _$Unexpected;
+}
+
+/// @nodoc
+
+class _$Unexpected implements Unexpected {
+  const _$Unexpected();
+
+  @override
+  String toString() {
+    return 'NoteFailure.unexpected()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$Unexpected);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() unexpected,
+    required TResult Function() insufficientPermission,
+    required TResult Function() unableToUpdate,
+    required TResult Function() unableToDelete,
+  }) {
+    return unexpected();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+  }) {
+    return unexpected?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+    required TResult orElse(),
+  }) {
+    if (unexpected != null) {
+      return unexpected();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Unexpected value) unexpected,
+    required TResult Function(InsufficientPermission value)
+        insufficientPermission,
+    required TResult Function(UnableToUpdate value) unableToUpdate,
+    required TResult Function(UnableToDelete value) unableToDelete,
+  }) {
+    return unexpected(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+  }) {
+    return unexpected?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+    required TResult orElse(),
+  }) {
+    if (unexpected != null) {
+      return unexpected(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class Unexpected implements NoteFailure {
+  const factory Unexpected() = _$Unexpected;
+}
+
+/// @nodoc
+abstract class _$$InsufficientPermissionCopyWith<$Res> {
+  factory _$$InsufficientPermissionCopyWith(_$InsufficientPermission value,
+          $Res Function(_$InsufficientPermission) then) =
+      __$$InsufficientPermissionCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$InsufficientPermissionCopyWithImpl<$Res>
+    extends _$NoteFailureCopyWithImpl<$Res>
+    implements _$$InsufficientPermissionCopyWith<$Res> {
+  __$$InsufficientPermissionCopyWithImpl(_$InsufficientPermission _value,
+      $Res Function(_$InsufficientPermission) _then)
+      : super(_value, (v) => _then(v as _$InsufficientPermission));
+
+  @override
+  _$InsufficientPermission get _value =>
+      super._value as _$InsufficientPermission;
+}
+
+/// @nodoc
+
+class _$InsufficientPermission implements InsufficientPermission {
+  const _$InsufficientPermission();
+
+  @override
+  String toString() {
+    return 'NoteFailure.insufficientPermission()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$InsufficientPermission);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() unexpected,
+    required TResult Function() insufficientPermission,
+    required TResult Function() unableToUpdate,
+    required TResult Function() unableToDelete,
+  }) {
+    return insufficientPermission();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+  }) {
+    return insufficientPermission?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+    required TResult orElse(),
+  }) {
+    if (insufficientPermission != null) {
+      return insufficientPermission();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Unexpected value) unexpected,
+    required TResult Function(InsufficientPermission value)
+        insufficientPermission,
+    required TResult Function(UnableToUpdate value) unableToUpdate,
+    required TResult Function(UnableToDelete value) unableToDelete,
+  }) {
+    return insufficientPermission(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+  }) {
+    return insufficientPermission?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+    required TResult orElse(),
+  }) {
+    if (insufficientPermission != null) {
+      return insufficientPermission(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class InsufficientPermission implements NoteFailure {
+  const factory InsufficientPermission() = _$InsufficientPermission;
+}
+
+/// @nodoc
+abstract class _$$UnableToUpdateCopyWith<$Res> {
+  factory _$$UnableToUpdateCopyWith(
+          _$UnableToUpdate value, $Res Function(_$UnableToUpdate) then) =
+      __$$UnableToUpdateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$UnableToUpdateCopyWithImpl<$Res>
+    extends _$NoteFailureCopyWithImpl<$Res>
+    implements _$$UnableToUpdateCopyWith<$Res> {
+  __$$UnableToUpdateCopyWithImpl(
+      _$UnableToUpdate _value, $Res Function(_$UnableToUpdate) _then)
+      : super(_value, (v) => _then(v as _$UnableToUpdate));
+
+  @override
+  _$UnableToUpdate get _value => super._value as _$UnableToUpdate;
+}
+
+/// @nodoc
+
+class _$UnableToUpdate implements UnableToUpdate {
+  const _$UnableToUpdate();
+
+  @override
+  String toString() {
+    return 'NoteFailure.unableToUpdate()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$UnableToUpdate);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() unexpected,
+    required TResult Function() insufficientPermission,
+    required TResult Function() unableToUpdate,
+    required TResult Function() unableToDelete,
+  }) {
+    return unableToUpdate();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+  }) {
+    return unableToUpdate?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+    required TResult orElse(),
+  }) {
+    if (unableToUpdate != null) {
+      return unableToUpdate();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Unexpected value) unexpected,
+    required TResult Function(InsufficientPermission value)
+        insufficientPermission,
+    required TResult Function(UnableToUpdate value) unableToUpdate,
+    required TResult Function(UnableToDelete value) unableToDelete,
+  }) {
+    return unableToUpdate(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+  }) {
+    return unableToUpdate?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+    required TResult orElse(),
+  }) {
+    if (unableToUpdate != null) {
+      return unableToUpdate(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class UnableToUpdate implements NoteFailure {
+  const factory UnableToUpdate() = _$UnableToUpdate;
+}
+
+/// @nodoc
+abstract class _$$UnableToDeleteCopyWith<$Res> {
+  factory _$$UnableToDeleteCopyWith(
+          _$UnableToDelete value, $Res Function(_$UnableToDelete) then) =
+      __$$UnableToDeleteCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$UnableToDeleteCopyWithImpl<$Res>
+    extends _$NoteFailureCopyWithImpl<$Res>
+    implements _$$UnableToDeleteCopyWith<$Res> {
+  __$$UnableToDeleteCopyWithImpl(
+      _$UnableToDelete _value, $Res Function(_$UnableToDelete) _then)
+      : super(_value, (v) => _then(v as _$UnableToDelete));
+
+  @override
+  _$UnableToDelete get _value => super._value as _$UnableToDelete;
+}
+
+/// @nodoc
+
+class _$UnableToDelete implements UnableToDelete {
+  const _$UnableToDelete();
+
+  @override
+  String toString() {
+    return 'NoteFailure.unableToDelete()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$UnableToDelete);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() unexpected,
+    required TResult Function() insufficientPermission,
+    required TResult Function() unableToUpdate,
+    required TResult Function() unableToDelete,
+  }) {
+    return unableToDelete();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+  }) {
+    return unableToDelete?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? unexpected,
+    TResult Function()? insufficientPermission,
+    TResult Function()? unableToUpdate,
+    TResult Function()? unableToDelete,
+    required TResult orElse(),
+  }) {
+    if (unableToDelete != null) {
+      return unableToDelete();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Unexpected value) unexpected,
+    required TResult Function(InsufficientPermission value)
+        insufficientPermission,
+    required TResult Function(UnableToUpdate value) unableToUpdate,
+    required TResult Function(UnableToDelete value) unableToDelete,
+  }) {
+    return unableToDelete(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+  }) {
+    return unableToDelete?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Unexpected value)? unexpected,
+    TResult Function(InsufficientPermission value)? insufficientPermission,
+    TResult Function(UnableToUpdate value)? unableToUpdate,
+    TResult Function(UnableToDelete value)? unableToDelete,
+    required TResult orElse(),
+  }) {
+    if (unableToDelete != null) {
+      return unableToDelete(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class UnableToDelete implements NoteFailure {
+  const factory UnableToDelete() = _$UnableToDelete;
+}
