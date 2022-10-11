3553d5daf616eed748693c2f4757f16663e5aa34
jan cahlik <honzicekcahlicek1@gmail.com>
2022-10-11 17:35:02 +0200

    note form widget

--- lib/presentation/pages/note_form/note_form_page.dart
+++ lib/presentation/pages/note_form/note_form_page.dart
@@ -1,10 +1,53 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:getwidget/components/button/gf_button.dart';
+import 'package:notes/presentation/ui_constants.dart';
+import 'package:notes/translations_constants.dart';
 
 class NoteFormPage extends StatelessWidget {
-  const NoteFormPage({Key? key}) : super(key: key);
+  NoteFormPage({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormBuilderState>();
 
   @override
   Widget build(BuildContext context) {
-    return Text("ahoj");
+    return Scaffold(
+      body: SafeArea(
+        minimum: const EdgeInsets.symmetric(vertical: UIConstants.safeAreaPaddingVertical, horizontal: UIConstants.safeAreaPaddingHorizontal),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(child: Text(TranslationsConstants.addNewNote).tr()),
+            FormBuilder(
+              key: _formKey,
+              child:  FormBuilderTextField(
+                name: 'text',
+                onChanged: (val) {
+                  print(val); // Print the text value write into TextField
+                },
+              ),
+            ),
+            FormBuilderCheckbox(name: 'ag', title: Text('ahoj')),
+            FormBuilderChoiceChip(name: 'ff', options: [
+                FormBuilderChipOption<String>(value: 'ahoj'),
+                FormBuilderChipOption<String>(value: 'cau'),
+                FormBuilderChipOption<String>(value: 'ano'),
+                FormBuilderChipOption<String>(value: 'nee')
+              ],
+            ),
+            GFButton(
+              fullWidthButton: true,
+              text: TranslationsConstants.addNewNote.tr(),
+              onPressed: () {
+                // todo this
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
